6616f953e5454c306521115c7cea6cd7c10b6025
Aurangzeb Khan <aurangzebbbk@gmail.com>
2025-02-18 20:25:56 +0500

    resolving issues

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import CalFunction from "./components/cal";
 import Footer from "./components/footer";
-import Banner from "./home/banner";
+import Banner from "./home/banner.js";
 import ProjectsTile from "./home/projectsTile";
 
 export default function Home() {
