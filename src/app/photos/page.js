5b95c96b693d3852cd45a1b61d0603ccd45a2aee
Aurangzeb Khan <aurangzebbbk@gmail.com>
2025-02-18 20:39:18 +0500

    Photos added

---
+++ src/app/photos/page.js
@@ -0,0 +1,11 @@
+import PageTitle from "./page-title";
+import PhotosGallery from "./photos-gallery";
+
+export default function PhotosMain() {
+  return (
+    <>
+      <PageTitle />
+      <PhotosGallery />
+    </>
+  );
+}
