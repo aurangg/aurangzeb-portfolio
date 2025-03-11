80097b61338bac25eac61646cef19c397c235458
Aurangzeb Khan <aurangzebbbk@gmail.com>
2025-03-11 22:54:16 +0500

    fixes

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -3,8 +3,31 @@ import CalFunction from "../components/cal";
 import Footer from "../components/footer";
 import ProjectsComponent from "../work/projects";
 import Padding from "../components/Padding";
+import { createClient } from "@prismicio/client";
 
-export default function HomePage({ homepage }) {
+export async function generateMetadata({ homepage }) {
+  return {
+    title:
+      homepage?.data.meta_title ||
+      "Sr. UI/UX Designer - Landing Pages | Dashboards | SaaS | Websites | Mobile",
+    description:
+      homepage?.data.meta_description || "Default description for SEO.",
+    openGraph: {
+      title: homepage?.data.meta_title,
+      description: homepage?.data.meta_description,
+
+      // openGraph: {
+      //   images: [{ url: homepage?.data.meta_image?.url }],
+      // },
+    },
+  };
+}
+
+export default async function HomePage({ homepage }) {
+  const client = createClient();
+  const homepage = await client.getSingle("homepage");
+  generateMetadata(homepage);
+  console.log(homepage);
   return (
     <main className="position-relative">
       <Padding spacing={40} border={true} />
--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,7 @@
-import { createClient } from "@/prismicio";
-import HomePage from "./home/page";
-
-export async function generateMetadata({ homepage }) {
-  return {
-    title:
-      homepage?.data.meta_title ||
-      "Sr. UI/UX Designer - Landing Pages | Dashboards | SaaS | Websites | Mobile",
-    description:
-      homepage?.data.meta_description || "Default description for SEO.",
-    openGraph: {
-      title: homepage?.data.meta_title,
-      description: homepage?.data.meta_description,
-
-      // openGraph: {
-      //   images: [{ url: homepage?.data.meta_image?.url }],
-      // },
-    },
-  };
-}
-
-export default async function Home() {
-  const client = createClient();
-  const homepage = await client.getSingle("homepage");
-  generateMetadata(homepage);
-  console.log(homepage);
+export default function Home() {
   return (
     <>
-      <HomePage homepage={homepage} />
+      <HomePage />
     </>
   );
 }
