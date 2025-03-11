69e95b977b2eeb7796b338cc8eb0f10985886d34
Aurangzeb Khan <aurangzebbbk@gmail.com>
2025-03-11 22:56:31 +0500

    declaration issue

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -15,19 +15,14 @@ export async function generateMetadata({ homepage }) {
     openGraph: {
       title: homepage?.data.meta_title,
       description: homepage?.data.meta_description,
-
-      // openGraph: {
-      //   images: [{ url: homepage?.data.meta_image?.url }],
-      // },
     },
   };
 }
 
-export default async function HomePage({ homepage }) {
+export default async function HomePage() {
   const client = createClient();
   const homepage = await client.getSingle("homepage");
   generateMetadata(homepage);
-  console.log(homepage);
   return (
     <main className="position-relative">
       <Padding spacing={40} border={true} />
