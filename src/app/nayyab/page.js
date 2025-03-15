0ae5d3623fc66f8926e72b12651d189f224bf74e
Aurangzeb Khan <aurangzebbbk@gmail.com>
2025-03-15 11:29:23 +0500

    demo page addition v3

--- src/app/nayyab/page.js
+++ src/app/nayyab/page.js
@@ -27,27 +27,32 @@ export default function Nayyab() {
                 className="banner-description"
                 style={{ marginBottom: "12px", opacity: "0.8" }}
               >
-                I hope this makes you feel better. Nothing but love, and so much
+                {`I hope this makes you feel better. Nothing but love, and so much
                 of it. ♥️ Itna dil pay na lain. Kuch kehnay ka dil kar raha hai,
-                laken likha nahi jaa raha, tou{" "}
+                laken likha nahi jaa raha, tou`}{" "}
                 <span>
-                  <Link href={`/${contact_page}`}>Contact Page</Link>
+                  <Link
+                    style={{ color: "red", textDecoration: "underline" }}
+                    href={`/${contact_page}`}
+                  >
+                    Contact Page
+                  </Link>
                 </span>{" "}
-                pay jayen, wahan bus aapnay dil ki saari baat likh dain. Agar wo
+                {`pay jayen, wahan bus aapnay dil ki saari baat likh dain. Agar wo
                 dil nahi, tou bus mujay aapnay pas sammjhain, bohat bohat bohat
-                pas. I'm here and I won't let you fall. I'm here!
+                pas. I'm here and I won't let you fall. I'm here!`}
               </p>
               <p
                 className="banner-description"
                 style={{ marginBottom: "12px", opacity: "0.8" }}
               >
-                Made with love, for you.
+                {`Made with love, for you.`}
               </p>
               <p
                 className="banner-description"
                 style={{ marginBottom: "12px", opacity: "0.8" }}
               >
-                Tons of hugssssssss!!! ♥️
+                {`Tons of hugssssssss!!! ♥️`}
               </p>
             </div>
           </div>
