a446c6fd6e3d5c97c3771df654bfaf9c6d10635f
Aurangzeb Khan <aurangzebbbk@gmail.com>
2025-03-15 11:23:48 +0500

    demo page addition v2

--- src/app/nayyab/page.js
+++ src/app/nayyab/page.js
@@ -18,9 +18,15 @@ export default function Nayyab() {
         />
         <div className="row">
           <div className="col-lg-12">
-            <div className="banner-box">
+            <div
+              className="banner-box"
+              style={{ flexDirection: "column", alignItems: "flex-start" }}
+            >
               <h1 className="banner-title">Hi Nayyab</h1>
-              <p className="banner-description">
+              <p
+                className="banner-description"
+                style={{ marginBottom: "12px", opacity: "0.8" }}
+              >
                 I hope this makes you feel better. Nothing but love, and so much
                 of it. ♥️ Itna dil pay na lain. Kuch kehnay ka dil kar raha hai,
                 laken likha nahi jaa raha, tou{" "}
@@ -29,10 +35,20 @@ export default function Nayyab() {
                 </span>{" "}
                 pay jayen, wahan bus aapnay dil ki saari baat likh dain. Agar wo
                 dil nahi, tou bus mujay aapnay pas sammjhain, bohat bohat bohat
-                pas. I'm here and I won't let you fall. I'm here! ♥️
+                pas. I'm here and I won't let you fall. I'm here!
               </p>
-              <p className="banner-description">Made with love, for you. ♥️</p>
-              <p className="banner-description">Tons of hugssssssss!!! ♥️</p>
+              <p
+                className="banner-description"
+                style={{ marginBottom: "12px", opacity: "0.8" }}
+              >
+                Made with love, for you.
+              </p>
+              <p
+                className="banner-description"
+                style={{ marginBottom: "12px", opacity: "0.8" }}
+              >
+                Tons of hugssssssss!!! ♥️
+              </p>
             </div>
           </div>
         </div>
